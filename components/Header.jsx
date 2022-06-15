a271347d1ff6b2c32b15bef1c2c121dbaf093449
Fabien Chareun <cacrfabfr@gmail.com>
2022-06-15 11:31:56 +0200

    Header done

---
+++ components/Header.jsx
@@ -0,0 +1,75 @@
+import Image from 'next/image'
+import {
+  SearchIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon,
+} from '@heroicons/react/outline'
+import { HomeIcon } from '@heroicons/react/solid'
+
+const Header = () => {
+  return (
+    <div className="shadow-sm border-b bg-white sticky top-0 z-50">
+      <div className="flex justify-between max-w-6xl mx-5 xl:mx-auto">
+        {/* Logo for big screen */}
+        <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
+          <Image
+            src="https://links.papareact.com/ocw"
+            alt="logo"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+
+        {/* Logo for small screen */}
+        <div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
+          <Image
+            src="https://links.papareact.com/jjm"
+            alt="logo"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+
+        {/* Search bar */}
+        <div className="max-w-xs">
+          <div className="mt-1 p-3 rounded-md relative ">
+            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+              <SearchIcon className="h-5 w-5 text-gray-500" />
+            </div>
+            <input
+              className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 rounded-md focus:ring-black focus:border-black"
+              type="text"
+              placeholder="Search"
+            />
+          </div>
+        </div>
+
+        {/* Buttons group right */}
+        <div className="flex items-center justify-end space-x-4">
+          <HomeIcon className="navBtn" />
+          <MenuIcon className="h-6 md:hidden cursor-pointer" />
+          <div className="relative navBtn">
+            <PaperAirplaneIcon className="rotate-45" />
+            <div className="absolute -top-2 -right-3 text-sm w-5 h-5 text-white bg-red-500 rounded-full flex items-center justify-center">
+              2
+            </div>
+          </div>
+          <PlusCircleIcon className="navBtn" />
+          <UserGroupIcon className="navBtn" />
+          <HeartIcon className="navBtn" />
+
+          <img
+            src="https://images.unsplash.com/photo-1457449940276-e8deed18bfff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            alt="profil pic"
+            className="h-10 w-10 cursor-pointer rounded-full"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
