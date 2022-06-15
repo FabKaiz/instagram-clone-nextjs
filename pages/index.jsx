7cc1232f832c8f830a6a44f1c59f8ca3387dc706
Fabien Chareun <cacrfabfr@gmail.com>
2022-06-15 13:47:57 +0200

    added css and added Feed

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,17 @@
 import Head from 'next/head'
+import Feed from '../components/Feed'
 import Header from '../components/Header'
 
 const Home = () => {
   return (
-    <div className="">
+    <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
       <Head>
         <title>Instagram 2.0 - FabKaiz</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
+      <Feed />
     </div>
   )
 }
