9d8b400bfd49e58bbcf390f284abbbf6292ee506
Fabien Chareun <cacrfabfr@gmail.com>
2022-06-15 14:36:23 +0200

    changed width

--- components/Header.jsx
+++ components/Header.jsx
@@ -63,7 +63,7 @@ const Header = () => {
           <HeartIcon className="navBtn" />
 
           <img
-            src="https://images.unsplash.com/photo-1457449940276-e8deed18bfff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            src="https://images.unsplash.com/photo-1457449940276-e8deed18bfff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=470&q=80"
             alt="profil pic"
             className="h-10 w-10 cursor-pointer rounded-full"
           />
