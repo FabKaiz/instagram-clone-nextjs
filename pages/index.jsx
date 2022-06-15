a59a148545ebb570bf40eb20540758413d0b9d7e
Fabien Chareun <cacrfabfr@gmail.com>
2022-06-15 10:24:02 +0200

    Initial commit from Create Next App

---
+++ pages/index.jsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+const Home = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Instagram 2.0 - FabKaiz</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h2>Intagram 2.0</h2>
+    </div>
+  )
+}
+
+export default Home
