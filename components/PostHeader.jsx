2694ebfe00a2d6a8cdfe7afcf366704a26a69666
Fabien Chareun <cacrfabfr@gmail.com>
2022-06-15 14:37:21 +0200

    format

--- components/PostHeader.jsx
+++ components/PostHeader.jsx
@@ -1,16 +1,16 @@
-import { DotsHorizontalIcon } from "@heroicons/react/outline"
+import { DotsHorizontalIcon } from '@heroicons/react/outline'
 
 const PostHeader = ({ userImg, username }) => {
   return (
     <div className="flex items-center p-5">
-    <img
-      className="rounded-full h-12 w-12 object-cover border p-1 mr-3"
-      src={userImg}
-      alt="user pic"
-    />
-    <p className="flex-1 font-bold">{username}</p>
-    <DotsHorizontalIcon className="h-5" />
-  </div>
+      <img
+        className="rounded-full h-12 w-12 object-cover border p-1 mr-3"
+        src={userImg}
+        alt="user pic"
+      />
+      <p className="flex-1 font-bold">{username}</p>
+      <DotsHorizontalIcon className="h-5" />
+    </div>
   )
 }
 
