c0880f3b8b82f8624f7cabcdf2d8cde26d3d8fd9
Fabien Chareun <cacrfabfr@gmail.com>
2022-06-15 13:47:36 +0200

    Added Stories component

---
+++ components/Stories.jsx
@@ -0,0 +1,31 @@
+import { faker } from '@faker-js/faker'
+import { useEffect, useState } from 'react'
+import Story from './Story'
+
+const Stories = () => {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    const usersList = [...Array(20)].map(() => ({
+      userId: faker.datatype.uuid(),
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      avatar: faker.image.avatar(),
+      password: faker.internet.password(),
+      birthdate: faker.date.birthdate(),
+      registeredAt: faker.date.past(),
+    }))
+
+    setUsers(usersList)
+  }, [])
+
+  return (
+    <div className="flex space-x-3 p-4 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black">
+      {users.map((user) => (
+        <Story key={user.userId} img={user.avatar} username={user.username} />
+      ))}
+    </div>
+  )
+}
+
+export default Stories
