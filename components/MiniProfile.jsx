31487cd1d9ce93c586da20e73c79614904b1e549
Fabien Chareun <cacrfabfr@gmail.com>
2022-06-16 15:42:06 +0200

    added user image and info

--- components/MiniProfile.jsx
+++ components/MiniProfile.jsx
@@ -1,18 +1,22 @@
+import { useSession, signOut } from 'next-auth/react'
+
 const MiniProfile = () => {
+  const { data: session } = useSession()
+
   return (
     <div className="flex items-center justify-between mt-14 ml-10">
       <img
-        src="https://images.unsplash.com/photo-1457449940276-e8deed18bfff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=470&q=80"
+        src={session?.user?.image}
         alt="profil pic"
         className="h-16 w-16 border p-[2px] cursor-pointer rounded-full"
       />
-
       <div className="flex-1 mx-4">
-        <h2 className="font-bold">fab_citron</h2>
-        <h3 className="text-sm text-gray-400">Welcome to instagram</h3>
+        <h2 className="font-bold">{session?.user?.username}</h2>
+        <h3 className="text-sm text-gray-400">{session?.user?.name}</h3>
       </div>
-
-      <button className="text-blue-400 text-sm font-semibold">Sign Out</button>
+      <button onClick={signOut} className="text-blue-400 text-sm font-semibold">
+        Sign Out
+      </button>
     </div>
   )
 }
