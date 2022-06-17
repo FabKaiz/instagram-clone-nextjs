08102e7ca446ae4c479dfdc867753fd52b72155e
Fabien Chareun <cacrfabfr@gmail.com>
2022-06-17 12:55:56 +0200

    added modal to log out

--- components/NavMenu.jsx
+++ components/NavMenu.jsx
@@ -3,12 +3,15 @@ import {
   PaperAirplaneIcon,
   PlusCircleIcon,
   UserGroupIcon,
+  LogoutIcon,
 } from '@heroicons/react/outline'
 import { HomeIcon } from '@heroicons/react/solid'
 import { signOut, useSession } from 'next-auth/react'
+import { useState } from 'react'
 
 const NavMenu = () => {
   const { data: session } = useSession()
+  const [toggleMenu, setToggleMenu] = useState(false)
 
   return (
     <div className="flex items-center justify-end space-x-4">
@@ -25,12 +28,28 @@ const NavMenu = () => {
           <UserGroupIcon className="navBtn" />
           <HeartIcon className="navBtn" />
 
-          <img
-            onClick={signOut}
-            src={session?.user?.image}
-            alt="profil pic"
-            className="h-10 w-10 cursor-pointer rounded-full"
-          />
+          <div className="relative">
+            <img
+              onClick={() => setToggleMenu(!toggleMenu)}
+              src={session?.user?.image}
+              alt="profil pic"
+              className=" h-10 w-10 cursor-pointer rounded-full"
+            />
+            {toggleMenu && (
+              <div className="absolute z-20 bg-white py-2 px-4 top-11 right-0 border drop-shadow-lg rounded w-36">
+                <button onClick={signOut} className="flex items-center">
+                  <LogoutIcon className="w-4" />
+                  <p className="ml-2">Log Out</p>
+                </button>
+              </div>
+            )}
+          </div>
+          {toggleMenu && (
+            <div
+              onClick={() => setToggleMenu(false)}
+              className="absolute top-0 right-0 bg-black/10 w-screen h-screen"
+            ></div>
+          )}
         </>
       ) : (
         <button onClick={signIn}>Sign In</button>
