73022589011a5a8b4f6cfe6b870d5ef3d8eedebd
Fabien Chareun <cacrfabfr@gmail.com>
2022-06-17 11:33:55 +0200

    Added current user to stories

--- components/Stories.jsx
+++ components/Stories.jsx
@@ -1,8 +1,10 @@
 import { faker } from '@faker-js/faker'
 import { useEffect, useState } from 'react'
 import Story from './Story'
+import { useSession } from 'next-auth/react'
 
 const Stories = () => {
+  const { data: session } = useSession()
   const [users, setUsers] = useState([])
 
   useEffect(() => {
@@ -17,6 +19,17 @@ const Stories = () => {
 
   return (
     <div className="flex space-x-3 p-4 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black">
+      {session && (
+        <div className="relative">
+          <Story
+            img={session?.user?.image}
+            username={session?.user?.username}
+          />
+          <span class="absolute right-[2px] top-[32px] border-2 border-white inline-flex items-center justify-center px-[4.4px] pt-[0px] text-md font-bold leading-none text-red-100 bg-blue-500 rounded-full">
+            <div className="pb-[3px]">+</div>
+          </span>
+        </div>
+      )}
       {users.map((user) => (
         <Story key={user.userId} img={user.avatar} username={user.username} />
       ))}
--- components/Story.jsx
+++ components/Story.jsx
@@ -1,6 +1,6 @@
 const Story = ({ img, username }) => {
   return (
-    <div className="">
+    <div>
       <img
         className="h-14 w-14 rounded-full p-[1.5px] border-2 border-red-400 object-contain cursor-pointer transition-transform duration-200 ease-out hover:scale-110"
         src={img}
