84f71c5510478a785d268740b305fc014b07c476
Fabien Chareun <cacrfabfr@gmail.com>
2022-06-17 12:56:09 +0200

    remane class to classname

--- components/Stories.jsx
+++ components/Stories.jsx
@@ -25,7 +25,7 @@ const Stories = () => {
             img={session?.user?.image}
             username={session?.user?.username}
           />
-          <span class="absolute right-[2px] top-[32px] border-2 border-white inline-flex items-center justify-center px-[4.4px] pt-[0px] text-md font-bold leading-none text-red-100 bg-blue-500 rounded-full">
+          <span className="absolute right-[2px] top-[32px] border-2 border-white inline-flex items-center justify-center px-[4.4px] pt-[0px] text-md font-bold leading-none text-red-100 bg-blue-500 rounded-full">
             <div className="pb-[3px]">+</div>
           </span>
         </div>
