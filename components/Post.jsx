9731d93342d0a2e18d4e623ca2b720cb88838ecf
Fabien Chareun <cacrfabfr@gmail.com>
2022-06-20 14:58:46 +0200

    added post timestamp

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,12 +1,13 @@
 import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
 import { useEffect, useState } from 'react'
+import Moment from 'react-moment'
 import { db } from '../firebase'
 import PostButtons from './PostButtons'
 import PostComments from './PostComments'
 import PostHeader from './PostHeader'
 import PostInputBox from './PostInputBox'
 
-const Post = ({ id, username, userImg, img, caption }) => {
+const Post = ({ id, username, userImg, img, caption, timestamp }) => {
   const [comments, setComments] = useState([])
 
   useEffect(
@@ -20,20 +21,26 @@ const Post = ({ id, username, userImg, img, caption }) => {
           setComments(snapshot.docs)
         }
       ),
-    [db]
+    [db, id]
   )
   return (
     <div className="bg-white my-7 border rounded-lg">
       <PostHeader username={username} userImg={userImg} />
 
-      <img className="object-cover w-full" src={img} alt="image" />
-      <PostButtons />
-      {/* likes */}
-      <div className="pl-5 py-1 truncate">
+      <img
+        className="sm:max-h-[680px] max-h-[490px] object-contain w-full"
+        src={img}
+        alt="image"
+      />
+      <PostButtons id={id} />
+      <div className="pl-3 py-1 truncate">
         <span className="font-bold mr-1">{username}</span>
         {caption}
       </div>
       <PostComments comments={comments} />
+      <Moment fromNow className="ml-3 text-gray-400 text-xs">
+        {timestamp}
+      </Moment>
       <PostInputBox id={id} />
     </div>
   )
