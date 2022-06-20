1efb8e7020194dcefc68cf6de63c8cf58bc0ddb4
Fabien Chareun <cacrfabfr@gmail.com>
2022-06-20 14:59:09 +0200

    added functionality to the post btns

--- components/PostButtons.jsx
+++ components/PostButtons.jsx
@@ -5,17 +5,55 @@ import {
   PaperAirplaneIcon,
 } from '@heroicons/react/outline'
 import { HeartIcon as HeartIconFilled } from '@heroicons/react/solid'
+import { collection, deleteDoc, doc, onSnapshot, setDoc } from 'firebase/firestore'
+import { useSession } from 'next-auth/react'
+import { useEffect, useState } from 'react'
+import { db } from '../firebase'
 
-const PostButtons = () => {
+const PostButtons = ({ id }) => {
+  const { data: session } = useSession()
+  const [likes, setLikes] = useState([])
+  const [hasLiked, setHasLiked] = useState(false)
+
+  useEffect(() => {
+    onSnapshot(collection(db, 'posts', id, 'likes'), (snapshot) =>
+      setLikes(snapshot.docs)
+    )
+  }, [db, id])
+
+  useEffect(() => {
+    setHasLiked(
+      likes.findIndex((like) => like.id === session?.user?.uid) !== -1
+    )
+  }, [likes])
+
+  const likePost = async () => {
+    if (hasLiked) {
+      await deleteDoc(doc(db, 'posts', id, 'likes', session.user.uid))
+    } else {
+      await setDoc(doc(db, 'posts', id, 'likes', session.user.uid), {
+        username: session.user.username,
+      })
+    }
+  }
+
   return (
-    <div className="flex justify-between px-3 py-4">
-      <div className="flex space-x-4 items-center">
-        <HeartIcon className="btn" />
-        <ChatIcon className="btn" />
-        <PaperAirplaneIcon className="btn rotate-45 w-6 -mt-1" />
+    <div>
+      <div className="flex justify-between px-3 pt-4 pb-2">
+        <div className="flex space-x-4 items-center">
+          {hasLiked ? (
+            <HeartIconFilled onClick={likePost} className="btn text-red-500" />
+          ) : (
+            <HeartIcon onClick={likePost} className="btn" />
+          )}
+          <ChatIcon className="btn" />
+          <PaperAirplaneIcon className="btn rotate-45 w-6 -mt-1" />
+        </div>
+        <BookmarkAltIcon className="btn" />
       </div>
-
-      <BookmarkAltIcon className="btn" />
+        {likes.length > 0 && (
+          <p className="font-semibold text-sm ml-3">{likes.length} likes</p>
+        )}
     </div>
   )
 }
