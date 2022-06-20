09502919f7248915efb2e5e663b93bc96a45ef52
Fabien Chareun <cacrfabfr@gmail.com>
2022-06-20 11:27:21 +0200

    Add post and image to firebase function

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -3,8 +3,19 @@ import { modalState } from '../atoms/modalAtom'
 import { Dialog, Transition } from '@headlessui/react'
 import { Fragment, useRef, useState } from 'react'
 import { CameraIcon } from '@heroicons/react/solid'
+import { db, storage } from '../firebase'
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  updateDoc,
+} from 'firebase/firestore'
+import { useSession } from 'next-auth/react'
+import { ref, getDownloadURL, uploadString } from 'firebase/storage'
 
 const Modal = () => {
+  const { data: session } = useSession()
   const [open, setOpen] = useRecoilState(modalState)
   const filePickerRef = useRef(null)
   const captionRef = useRef(null)
@@ -17,7 +28,27 @@ const Modal = () => {
 
     setIsLoading(true)
 
-    // TODO: SETUP FIREBASE
+    const docRef = await addDoc(collection(db, 'posts'), {
+      username: session.user.username,
+      caption: captionRef.current.value,
+      profileImg: session.user.image,
+      timestamp: serverTimestamp(),
+    })
+
+    const imageRef = ref(storage, `posts/${docRef.id}/image`)
+
+    await uploadString(imageRef, selectedFile, 'data_url').then(
+      async (snapshot) => {
+        const downloadURL = await getDownloadURL(imageRef)
+        await updateDoc(doc(db, 'posts', docRef.id), {
+          image: downloadURL,
+        })
+      }
+    )
+
+    setOpen(false)
+    setIsLoading(false)
+    setSelectedFile(null)
   }
 
   const addImageToPost = (e) => {
@@ -124,8 +155,13 @@ const Modal = () => {
                   </div>
                 </div>
                 <div className="mt-5 sm:mt-6">
-                  <button className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-800 transition-colors duration-150 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed hover:disabled:bg-gray-300">
-                    Uplaod pic
+                  <button
+                    type="button"
+                    disabled={!selectedFile}
+                    onClick={uploadPost}
+                    className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-800 transition-colors duration-150 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed hover:disabled:bg-gray-300"
+                  >
+                    {isLoading ? 'Uploading Post...' : 'Upload Post'}
                   </button>
                 </div>
               </div>
